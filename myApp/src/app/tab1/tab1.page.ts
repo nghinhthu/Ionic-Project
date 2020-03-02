77516b77e7a0715df57dbd2f823edef5ea27ff99
nghinhthu <huahoangnghinhthu@gmail.com>
2020-03-02 11:14:28 +0700

    .

--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app'
+import { AlertController } from '@ionic/angular'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,44 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  userName: string = "";
+  password: string = "";
+  cPassword: string = "";
 
+  ngOninit() {
+
+  }
+
+  constructor(
+    public afAuth: AngularFireAuth, 
+    public alert: AlertController,
+    public router: Router) {}
+
+  async register(){
+    const { userName, password, cPassword} = this
+    if(this.password !== this.cPassword){
+      this.showAlert("Error", "Password do not match")
+      return console.log("Password do not match")
+    }
+    try{
+      const res = await this.afAuth.auth.createUserWithEmailAndPassword(userName + '@abc.com', password)
+      console.log(res)
+      this.showAlert("Success!", "Welcome Aboard!")
+      this.router.navigate(['/tab3'])
+    }
+    catch(error){
+      console.dir(error)
+      this.showAlert("Error", error.message)
+    }
+  }
+
+  async showAlert(header: string, message: string){
+    const alert = await this.alert.create({
+      header, 
+      message,
+      buttons: ["Ok"]
+    })
+
+    await alert.present()
+  }
 }

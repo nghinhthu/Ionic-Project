476d9d7345305cc3b9d97eb1a9bca57e19901b74
bapzzzbap <54621478+bapzzzbap@users.noreply.github.com>
2020-03-15 16:00:28 +0700

    functions

--- myApp/functions/src/index.ts
+++ myApp/functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+export const getFeed = functions.https.onCall(async(req, res) => {
+    const docs = await admin.firestore().collection('posts').limit(10).get()
+    return docs.docs.map(doc => doc.data())
+})
+

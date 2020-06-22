9f34e88b5fb388f8dfb91d733ec943a9c1cc0efe
nghinhthu <54279900+nghinhthu@users.noreply.github.com>
2020-06-22 14:40:11 +0700

    build duoc ruiiiiiii

--- myApp/src/app/firebase.ts
+++ myApp/src/app/firebase.ts
@@ -7,5 +7,5 @@ const firebaseConfig = {
     messagingSenderId: "354898369503",
     appId: "1:354898369503:web:ee39c45e862df6e22c826a",
     measurementId: "G-C9K347CJ18"
-  };
+};
 export default firebaseConfig 
--- myApp/src/environments/environment.ts
+++ myApp/src/environments/environment.ts
@@ -5,16 +5,16 @@
 export const environment = {
   production: false
 };
-const firebaseConfig = {
-  apiKey: "AIzaSyAdnKeNSQcowB6GzNmqfANg3iCSKwjrpMo",
-  authDomain: "ionic-project-e1801.firebaseapp.com",
-  databaseURL: "https://ionic-project-e1801.firebaseio.com",
-  projectId: "ionic-project-e1801",
-  storageBucket: "ionic-project-e1801.appspot.com",
-  messagingSenderId: "354898369503",
-  appId: "1:354898369503:web:ee39c45e862df6e22c826a",
-  measurementId: "G-C9K347CJ18"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAdnKeNSQcowB6GzNmqfANg3iCSKwjrpMo",
+//   authDomain: "ionic-project-e1801.firebaseapp.com",
+//   databaseURL: "https://ionic-project-e1801.firebaseio.com",
+//   projectId: "ionic-project-e1801",
+//   storageBucket: "ionic-project-e1801.appspot.com",
+//   messagingSenderId: "354898369503",
+//   appId: "1:354898369503:web:ee39c45e862df6e22c826a",
+//   measurementId: "G-C9K347CJ18"
+// };
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
